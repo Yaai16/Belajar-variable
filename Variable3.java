20b267deb0cc164911c61a3ec7e196a408bc492e
Yaai <101816183+Yaai16@users.noreply.github.com>
2023-09-16 23:00:28 +0700

    Add files via upload

---
+++ Variable3.java
@@ -0,0 +1,22 @@
+public class Variable3 {
+    public static void main(String[] args) {
+        // deklarasi variabel;
+        String nim = "2311016110012", nama = "Alvin William";
+       
+        
+        // tulis ke layar
+        System.out.println("Nim:  " + nim);
+        System.out.println("Nama: " + nama);
+
+        System.out.println("");
+
+        nim = "2311016110008"; nama = "Candra Permana" ;
+        System.out.println("Nim:  " + nim);
+        System.out.println("Nama: " + nama);
+
+
+  }
+}
+
+    
+
